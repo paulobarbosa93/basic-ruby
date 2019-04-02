3a0d235b2993e7d1c5deb336be9ba361953ac76c
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-04-02 10:21:01 -0300

    data types in ruby - part 1

---
+++ booleans.rb
@@ -0,0 +1,9 @@
+puts true.class
+puts false.class
+puts nil.class
+
+if nil
+  puts "nil is true"
+else
+  puts "nil is false"
+end
---
+++ symbols.rb
@@ -0,0 +1,9 @@
+puts :this_is_a_simbol.class
+
+puts :this_is_a_simbol.object_id
+puts :this_is_a_simbol.object_id
+puts :this_is_a_simbol.object_id
+
+puts 'this_is_a_string'.object_id
+puts 'this_is_a_string'.object_id
+puts 'this_is_a_string'.object_id
